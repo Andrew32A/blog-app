8520b837cfa4d50a0b232589b478c611b049651f
Andrew Alsing <andrew32alsing@gmail.com>
2023-05-16 14:23:41 -0700

    added crud for author

--- script.ts
+++ script.ts
@@ -2,21 +2,43 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
+    // delete all authors
     await prisma.author.deleteMany();
 
+    // [create] create author
     const author = await prisma.author.create({
         data: {
             username: "andrew",
             givenName: "andrew a",
         }
     });
-    
-    console.log(author)
-    // await prisma.author.findMany();
+    console.log("ORIGINAL AUTHOR: ", author)
+
+    // [read] finds all authors
+    const allAuthors = await prisma.author.findMany();
+    console.log("ALL AUTHORS: ", allAuthors)
+
+    // [update] updates user
+    const updatedAuthor = await prisma.author.update({
+        where: {
+            username: "andrew"
+        },
+        data: {
+            username: "updated username"
+        }
+    })
+    console.log("UPDATED AUTHOR: ", updatedAuthor)
+
+    // [delete] deletes user
+    const deleteAuthor = await prisma.author.delete({
+        where: {
+            username: "updated username"
+        }
+    })
+    console.log("CHECK FOR DELETED USER", allAuthors)
 }
 
-console.log("hello world");
-
+// prisma code from docs
 main()
   .then(async () => {
     await prisma.$disconnect();
